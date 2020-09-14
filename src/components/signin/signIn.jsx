6a126ec2df56c0e41a6c7a790bd5b52ae683c587
wassimnassour <nassourwassim@gmail.com>
2020-09-14 11:01:43 +0100

    add loading

--- src/components/signin/signIn.jsx
+++ src/components/signin/signIn.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { BoxLoading } from "react-loadingg";
+
 import {
   SignUpContainer,
   SignUpWrapper,
@@ -11,6 +13,7 @@ const SignIn = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     auth.onAuthStateChanged(async (_usr) => {
@@ -18,58 +21,72 @@ const SignIn = ({ history }) => {
 
       if (_usr) {
         history.push("/dashboard");
+      } else {
+        setLoading(false);
       }
     });
   }, [history]);
 
   const submit = () => {
     if (email.length > 0 && password.length > 0) {
-      console.log(email, password);
+      setLoading(true);
+
       auth
         .signInWithEmailAndPassword(email, password)
-        .then(() => history.push("/dashboard"))
-        .catch((err) => setError(err.message));
+        .then(() => {
+          history.push("/dashboard");
+        })
+        .catch((err) => {
+          setLoading(false);
+
+          setError(err.message);
+        });
     } else {
+      setLoading(false);
       setError("something went wrong ");
     }
   };
 
   return (
     <SignUpContainer>
-      <SignUpWrapper>
-        <h1>Sign In</h1>
-        <form>
-          {error ? (
-            <div>
-              <span className="danger">{error}</span>
-            </div>
-          ) : null}
-          <InputContainer>
-            <label>Email:</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </InputContainer>
-          <InputContainer>
-            <label>Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </InputContainer>
-        </form>
-        <button type="submit" onClick={() => submit()}>
-          Sign In
-        </button>
-        <span>
-          if you have don't have account <Link to="/signup">Sign Up</Link>
-        </span>
-      </SignUpWrapper>
+      {!loading ? (
+        <SignUpWrapper>
+          <h1>Sign In</h1>
+          <form>
+            {error ? (
+              <div>
+                <span className="danger">{error}</span>
+              </div>
+            ) : null}
+            <InputContainer>
+              <label>Email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </InputContainer>
+            <InputContainer>
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </InputContainer>
+          </form>
+          <button type="submit" onClick={() => submit()}>
+            Sign In
+          </button>
+          <span>
+            if you have don't have account <Link to="/signup">Sign Up</Link>
+          </span>
+        </SignUpWrapper>
+      ) : (
+        <BoxLoading CircleToBlockLoading color="#0028ff" />
+      )}
     </SignUpContainer>
   );
 };
